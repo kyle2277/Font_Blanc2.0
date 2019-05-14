f879e3f4d3db65e2f8f2cacf9f8951f6e811b00f
kyle2277 <kylejwon@gmail.com>
2019-05-13 21:52:25 -0700

    Modified for integration with Global Mogul

--- src/FontBlanc2Main.java
+++ src/FontBlanc2Main.java
@@ -3,36 +3,29 @@ import java.sql.Timestamp;
 
 public class FontBlanc2Main {
 
-    public static String file_path;
-    public static String file_name;
-    public static final String log_path = "./log.txt";
-    public static final String encrypt_tag = "encrypted_";
-    public static final String decrypt_tag = "decrypted_";
-    public static final String encrypt_extension = ".txt";
-
-    public static int main(String filePath, String encryptKey, String EorD) throws IOException {
+    public static int main(String filePath, String encryptKey, String EorD, String cwd) throws IOException {
         long startTime = System.currentTimeMillis();
-        splitPath(filePath);
-        long fileLength = checkThisFile();
-        Cipher c = new Cipher(encryptKey, fileLength);
+        Globals g = new Globals(filePath, cwd);
+        long fileLength = checkThisFile(g);
+        Cipher c = new Cipher(encryptKey, fileLength);;
         FileInputStream in = null;
         FileOutputStream out = null;
         int coeff = 0;
         try {
             if(EorD.equalsIgnoreCase("encrypt")) {
-                in = new FileInputStream(file_path + file_name);
-                out = new FileOutputStream(encrypt_tag + file_name + encrypt_extension);
+                in = new FileInputStream(g.file_path + g.file_name);
+                out = new FileOutputStream(g.file_path + g.encrypt_tag + g.file_name + g.encrypt_extension);
                 coeff = 1;
             } else if(EorD.equalsIgnoreCase("decrypt")) {
-                in = new FileInputStream(encrypt_tag + file_name + encrypt_extension);
-                out = new FileOutputStream(decrypt_tag + file_name);
+                in = new FileInputStream(g.encrypt_tag + g.file_path + g.file_name + g.encrypt_extension);
+                out = new FileOutputStream(g.file_path + g.decrypt_tag + g.file_name);
                 coeff = -1;
             } else {
-                return fatal("Invalid action");
+                return fatal(g,"Invalid action");
             }
             c.distributor(in, out, coeff);
         } catch(IOException e) {
-            return fatal("Output path not found");
+            return fatal(g, "Output path not found");
         } finally {
             if (in != null) { in.close(); }
             if (out != null) { out.close(); }
@@ -43,40 +36,15 @@ public class FontBlanc2Main {
     }
 
     /*
-    Splits the input file path into two components: path to the file name and the file name itself
-    */
-    public static void splitPath(String file_input) {
-        String[] split = file_input.split("/", 0);
-        int splitLen = split.length;
-        StringBuilder path = new StringBuilder();
-        file_path = "";
-        if(splitLen > 1) {
-            //set global file path var
-            for(int i = 0; i < splitLen - 1; i++) {
-                path.append("/");
-                path.append(split[i]);
-            }
-            path.append("/");
-        } else {
-            path.append("./");
-        }
-        file_path = path.toString();
-        //File name var
-        file_name = split[splitLen - 1];
-    }
-
-
-    /*
     Takes the path of the input file
     Checks if the input file exists and returns the length of the file in bytes
      */
-    public static long checkThisFile() throws IOException {
-        File f = new File(file_path + file_name);
+    public static long checkThisFile(Globals g) throws IOException {
+        File f = new File(g.file_path + g.file_name);
         if(f.exists()) {
             return f.length();
         } else {
-            fatal("Input file " + file_path + file_name + " path does not exist");
-            return -1;
+            return fatal(g, "Input file " + g.file_path + g.file_name + " path does not exist");
         }
     }
 
@@ -84,8 +52,8 @@ public class FontBlanc2Main {
     Triggered if a fatal error occurs. Writes the error to the console and log file
     before program termination
     */
-    public static int fatal(String message) throws IOException {
-        File fatal = new File(log_path);
+    public static int fatal(Globals g, String message) throws IOException {
+        File fatal = new File(g.log_path);
         FileWriter out = new FileWriter(fatal, true);
         Timestamp time = new Timestamp(System.currentTimeMillis());
         out.write(time + "\n");
@@ -94,7 +62,6 @@ public class FontBlanc2Main {
         System.out.println("Fatal error:");
         System.out.println(message);
         out.close();
-        System.exit(0);
-        return -1;
+        return 0;
     }
 }
---
+++ src/Globals.java
@@ -0,0 +1,40 @@
+public class Globals {
+
+    public String file_path;
+    public String file_name;
+    public String log_path;
+    public final String log_name = "../JNI/Font_Blanc2.0/log.txt";
+    public final String encrypt_tag = "encrypted_";
+    public final String decrypt_tag = "decrypted_";
+    public final String encrypt_extension = ".txt";
+
+    public Globals(String filePath, String cwd) {
+        log_path = cwd + "/" + log_name;
+        System.out.println(log_path);
+        splitPath(filePath);
+    }
+
+    /*
+    Splits the input file path into two components: path to the file name and the file name itself
+    */
+    public void splitPath(String file_input) {
+        String[] split = file_input.split("/", 0);
+        int splitLen = split.length;
+        StringBuilder path = new StringBuilder();
+        file_path = "";
+        if(splitLen > 1) {
+            //set global file path var
+            for(int i = 0; i < splitLen - 1; i++) {
+                path.append(split[i]);
+                path.append("/");
+            }
+            //path.append("/");
+        } else {
+            path.append("./");
+        }
+        file_path = path.toString();
+        //File name var
+        file_name = split[splitLen - 1];
+    }
+
+}
