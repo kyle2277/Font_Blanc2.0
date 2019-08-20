d40e56eb6303fb8ce76fcf15daa35e896ba2a809
kyle2277 <kylejwon@gmail.com>
2019-08-20 00:22:58 -0700

    File drag and drop. Add and remove to and from file list.

--- src/main/swing_gui/Font_Blanc2_App.java
+++ src/main/swing_gui/Font_Blanc2_App.java
@@ -4,36 +4,86 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.*;
 
 public class Font_Blanc2_App {
     private JButton openFileButton;
     private JPanel panelMain;
     private JTextArea DnD_area;
     private JTextArea statusArea;
+    private JButton resetButton;
+    private JList fileList;
+    private DefaultListModel lm;
+    //todo remove selected button
+    private JButton removeSelectedButton;
     private JFileChooser fc;
+    private HashMap<String, String> fileMap;
 
     public Font_Blanc2_App() {
 
+        fileMap = new LinkedHashMap<>();
+        lm = new DefaultListModel();
+        fileList.setModel(lm);
 
+        //files drag and dropped
+        new FileDrop(DnD_area, new FileDrop.Listener() {
+           public void filesDropped(File[] files) {
+               for(File f: files) {
+                   String name = f.getName();
+                   lm.addElement(name);
+                   fileMap.put(name, f.getAbsolutePath());
+               }
+           }
+        });
 
         //open file button
         fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
         openFileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 int returnVal = fc.showOpenDialog(panelMain);
                 if(returnVal == JFileChooser.APPROVE_OPTION) {
-                    File[] files = fc.getSelectedFiles();
-                    StringBuilder s = new StringBuilder("Files in queue:\n");
-                    for(File f: files) {
-                        s.append(f.getAbsolutePath());
-                        s.append("\n");
-                    }
-                    String out = s.toString();
-                    statusArea.setText(out);
+                    File f = fc.getSelectedFile();
+                    String name = f.getName();
+                    lm.addElement(name);
+                    fileMap.put(name, f.getAbsolutePath());
                 }
             }
         });
+
+        //reset button
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                resetMain();
+            }
+        });
+        removeSelectedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int[] ind = fileList.getSelectedIndices();
+                if(ind.length > 0) {
+                    int count = 0;
+                    for(int i: ind) {
+                        String fileName = (String) lm.getElementAt(i-count);
+                        lm.removeElementAt(i-count);
+                        fileMap.remove(fileName);
+                        count++;
+                    }
+                }
+            }
+        });
+    }
+
+    //reset the app home frame
+    public void resetMain() {
+        if(!lm.isEmpty()) {
+            lm.clear();
+        }
+        if(!fileMap.isEmpty()) {
+            fileMap.clear();
+        }
     }
 
     public static void main(String[] args) {
@@ -43,11 +93,6 @@ public class Font_Blanc2_App {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.pack();
         f.setVisible(true);
-        resetMain(app);
-
-    }
-
-    public static void resetMain(Font_Blanc2_App app) {
-        app.statusArea.setText("");
+        app.resetMain();
     }
 }
